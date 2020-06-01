45c882167b6e0c3aa7de6aaab22bb3184cea3409
holzerV <59916228+holzerV@users.noreply.github.com>
2020-06-01 13:38:02 +0300

    Updates

---
+++ precache-manifest.af86afaf68f27fe06a8027b81fddec6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5599064d2fdecb6fe7d31010bd9503",
+    "url": "/linkr/index.html"
+  },
+  {
+    "revision": "15bca4efe65f72669cb9",
+    "url": "/linkr/static/css/main.3ee8e672.chunk.css"
+  },
+  {
+    "revision": "ca164bd6de258315fbeb",
+    "url": "/linkr/static/js/2.e2d025ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/linkr/static/js/2.e2d025ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15bca4efe65f72669cb9",
+    "url": "/linkr/static/js/main.33c3b216.chunk.js"
+  },
+  {
+    "revision": "120d52bc7d7435fb193e",
+    "url": "/linkr/static/js/runtime-main.7ce6ade5.js"
+  }
+]);
